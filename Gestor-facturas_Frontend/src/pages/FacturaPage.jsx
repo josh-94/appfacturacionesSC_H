b866319cedbcc9d6bbbad08452c0120c11b919fb
yerti <yerti0818@gmail.com>
2024-01-09 11:45:55 -0500

    Feat(page): Agregar la pagina de detalles de la factura

---
+++ Gestor-facturas_Frontend/src/pages/FacturaPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import DetallesDeLaFactura from '../components/DetallesDeLaFactura'
+
+export default function FacturaPage() {
+  return (
+	<div>
+	  <DetallesDeLaFactura/>
+	</div>
+  )
+}
