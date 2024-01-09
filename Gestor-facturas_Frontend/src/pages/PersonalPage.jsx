d92aa3f8fc41af0c00b4745a6f0ec405ab94a8ab
yerti <yerti0818@gmail.com>
2024-01-09 11:45:10 -0500

    Feat(page): Agregar la pagina de personal

---
+++ Gestor-facturas_Frontend/src/pages/PersonalPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ListaDePersonal from '../components/ListaDePersonal'
+
+export default function PersonalPage() {
+    
+    return (
+        <div>
+            <ListaDePersonal/>
+        </div>
+    )
+}
