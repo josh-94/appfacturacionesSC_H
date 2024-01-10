46d881a6487d788e0e32cfe7d7c2b8b73b1d9271
Jose Suarez <jolusuvi@gmail.com>
2024-01-09 22:59:39 -0500

    Enlaces a las vistas de la API

--- Gestor-facturas_Backend/gestor_facturas/gestor_facturas/urls.py
+++ Gestor-facturas_Backend/gestor_facturas/gestor_facturas/urls.py
@@ -2,13 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from ..api.views import DocumentoVentaDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('api.urls')),
     path('api/', include('api.urls')),
-    path('documentodetail/<str:pk>/', DocumentoVentaDetailView.as_view(), name='documentodetail'),
 ]
 
 if settings.DEBUG:
