0ad6ab984f01fef5ff569b4df73e1302b0a3fc8e
yerti <yerti0818@gmail.com>
2024-01-10 23:14:23 -0500

    chore: actualizar imágenes

---
+++ Gestor-facturas_Frontend/src/components/imagenes.js
@@ -0,0 +1,29 @@
+import Check from '../assets/chek.svg'
+import FlechaAbajo from '../assets/flecha-abajo.svg'
+import Recarga from '../assets/refresh-two.svg'
+import Flecha from '../assets/chevron-down.svg'
+import Regreso from '../assets/Exclude.svg'
+import Persona from '../assets/persona-sin-foto.svg'
+import Logo from '../assets/logo-empresa.svg'
+import RecargaFactura from '../assets/reload-outline.svg'
+import RegresarPagina from '../assets/regresar-pagina.svg'
+import Enviado from '../assets/checkmark-circle-outline.svg'
+import FlechaActualizar from '../assets/Group-12.svg'
+import Eliminar from '../assets/eliminar.png'
+
+const imagenes =  {
+    "img-chek" : Check,
+    "img-flecha-abajo" : FlechaAbajo,
+    "img-recarga" : Recarga,
+    "img-flecha" : Flecha,
+    "img-regreso" : Regreso,
+    "img-persona" : Persona,
+    "img-logo-empresa" : Logo,
+    'img-recargar-factura' : RecargaFactura,
+    "img-regresar-pagina": RegresarPagina,
+    "img-enviado-check": Enviado,
+    "img-flecha-actilizar": FlechaActualizar,
+    "img-eliminar": Eliminar,
+}
+
+export default imagenes;
